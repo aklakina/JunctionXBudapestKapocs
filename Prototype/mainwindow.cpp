c4f2c35c2a788c5aaafa10a5285dcc46fe4c81a1
aklakina <54154570+aklakina@users.noreply.github.com>
2021-10-23 12:42:44 +0200

    1st

---
+++ Prototype/mainwindow.cpp
@@ -0,0 +1,57 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+unsigned short MainWindow::state=0;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    bool ok=false;
+    while (!ok) {
+        QString user = QInputDialog::getText(this, tr("Login"),
+                                             tr("User name:"), QLineEdit::Normal,
+                                             QDir::home().dirName(), &ok);
+        if (ok && !user.isEmpty()) {
+            ui->lineEdit->setText(user);
+        } else {
+            break;
+        }
+        QString pwd = QInputDialog::getText(this, tr("Login"),
+                                     tr("Password"), QLineEdit::Password,
+                                     QDir::home().dirName(), &ok);
+        if (ok && !pwd.isEmpty()) {
+            ui->lineEdit_2->setText(pwd);
+        } else {
+            break;
+        }
+        ui->lineEdit_2->setEchoMode(QLineEdit::Password);
+        if (user=="unemployed" && pwd=="uep") {
+            state=1;
+        } else if (user=="mentor" && pwd=="mt"){
+            state=2;
+        } else {
+            ok=false;
+            QMessageBox::warning(this, tr("Login"),
+                                           tr("Authentication failed"),
+                                           QMessageBox::Ok);
+        }
+    }
+    if (state==0) {
+        this->close();
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    this->close();
+}
+
---
+++ Prototype/mainwindow.h
@@ -0,0 +1,29 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QInputDialog>
+#include <QDir>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+private:
+    static unsigned short state;
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
